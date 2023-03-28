ba32fbb4c4cbdaa1c7d2e60cc00b08863922d4bb
simrnchahl <129125111+simrnchahl@users.noreply.github.com>
2023-03-28 07:05:02 +0530

    Create letter.py

---
+++ letter.py
@@ -0,0 +1,8 @@
+# Write code below 💖
+
+print("28 March 2023")
+print("I am feeling a bit tired from all that coding but excited to learn non the less")
+print("I will learn to code in python after I am done with this course")
+print("What my futureself should know is not to give up and keep pusing on.")
+print("Also typing on keyboard isn't as boring as I thought")
+print(":)")
